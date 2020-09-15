846aceab928c1d98d55c940992408df65a3cefa8
ZaytsevAlexander <mralexanderzaytsev@gmail.com>
2020-09-15 15:05:18 +0300

    refactor: made 2 header components from 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import './App.css';
 import { Layout } from 'antd';
 import Header from './components/header';
 
+import styles from './App.module.css';
+
 import TableContainer from './components/table-container';
 import CreateTask from './components/create-task';
+import TableHeader from './components/table-header';
 
 const { Content } = Layout;
 
 const App: FC = (): ReactElement => (
   <Layout>
-    <Header />
+    <div className={styles.header}>
+      <Header />
+      <TableHeader />
+    </div>
     <Content>
       <TableContainer />
     </Content>
---
+++ src/components/table-header/table-header-data.tsx
@@ -0,0 +1,79 @@
+import { IOptionItem } from '../../interfaces';
+
+const locations: Array<IOptionItem> = [
+    {
+        name: 'Europe/London',
+        id: 1,
+    },
+    {
+        name: 'Europe/Warsaw',
+        id: 2,
+    },
+    {
+        name: 'Europe/Kiev',
+        id: 3,
+    },
+    {
+        name: 'Europe/Minsk',
+        id: 4,
+    },
+    {
+        name: 'Europe/Moscow',
+        id: 5,
+    },
+    {
+        name: 'Europe/Volgograd',
+        id: 6,
+    },
+    {
+        name: 'Europe/Yekaterinburg',
+        id: 7,
+    },
+    {
+        name: 'Europe/Tashkent',
+        id: 8,
+    },
+    {
+        name: 'Europe/Tbilisi',
+        id: 9,
+    },
+];
+const defaultLocationIndex = 3;
+
+const displays: Array<IOptionItem> = [
+    {
+        name: 'Table',
+        id: 1
+    },
+    {
+        name: 'List',
+        id: 2
+    },
+    {
+        name: 'Calendar',
+        id: 3
+    },
+];
+const defaultDisplayIndex = 0;
+
+const courses: Array<IOptionItem> = [
+    {
+        name: 'RSS react 2020 Q3',
+        id: 1,
+    },
+    {
+        name: 'RSS 2020 Q1',
+        id: 2,
+    },
+    {
+        name: 'RSS angular 2020 Q3',
+        id: 3,
+    },
+    {
+        name: 'RSS node js 2020 Q3',
+        id: 4,
+    },
+]
+const defaultCourseIndex = 0;
+
+export { locations, defaultLocationIndex, displays, defaultDisplayIndex, courses, defaultCourseIndex };
