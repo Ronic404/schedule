657c42f4d7fbfd643d602b43a2e7d5c4cde34df5
ROnic404 <ronic404@MacBook-Pro-Aleksandr.local>
2020-09-16 22:26:24 +0400

    feat: add cards

--- src/components/list-structure/list-structure.tsx
+++ src/components/list-structure/list-structure.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { ReactElement } from 'react';
-import { List, Tag } from 'antd';
+import { List, Tag, Card } from 'antd';
 import data from '../data';
 import taskTypes from '../task-types';
 
@@ -7,26 +8,31 @@ import styles from './list-structure.module.css';
 
 export default function ListStructure(): ReactElement {
   return (
-    <List
-      className={styles.list}
-      itemLayout="horizontal"
-      dataSource={data}
-      bordered
-      size="small"
-      renderItem={(item) => (
-        <List.Item className={styles.item} key={item.key}>
-          <Tag
-            className={styles.type}
-            color={taskTypes.find((task) => task.value === item.type)?.color}
-          >
-            {item.type.toUpperCase()}
-          </Tag>
-          <div className={styles.main}>
-            <p className={styles.date}>{item.date}</p>
-            <p className={styles.name}>{item.name}</p>
-          </div>
-        </List.Item>
-      )}
-    />
+    <Card className={styles.list} title="Tasks list" bodyStyle={{ padding: 0 }}>
+      <List
+        // className={styles.list}
+        dataSource={data}
+        // bordered
+        // size="small"
+        renderItem={(item) => (
+          <Card.Grid className={styles.item} key={item.key}>
+            <Tag
+              className={styles.type}
+              color={taskTypes.find((task) => task.value === item.type)?.color}
+            >
+              {item.type.toUpperCase()}
+            </Tag>
+            <Card
+              className={styles.main}
+              type="inner"
+              title={item.date}
+              extra={<a href="#">More</a>}
+            >
+              {item.name}
+            </Card>
+          </Card.Grid>
+        )}
+      />
+    </Card>
   );
 }
